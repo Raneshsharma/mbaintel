eb332d1bbac375704651242232ade813616156fa
Raneshsharma <89022768+Raneshsharma@users.noreply.github.com>
2026-01-13 20:13:49 +0530

    Create colleges.js

---
+++ colleges.js
@@ -0,0 +1,38 @@
+const colleges = [
+  {
+    id: 1,
+    name: "IIM Ahmedabad",
+    tier: "Tier I",
+    location: "Ahmedabad, Gujarat",
+    fees: 28,
+    avgPackage: 31,
+    exams: ["CAT"]
+  },
+  {
+    id: 2,
+    name: "XLRI Jamshedpur",
+    tier: "Tier I",
+    location: "Jamshedpur, Jharkhand",
+    fees: 27,
+    avgPackage: 28,
+    exams: ["XAT", "GMAT"]
+  },
+  {
+    id: 3,
+    name: "IMT Ghaziabad",
+    tier: "Tier II",
+    location: "Ghaziabad, UP",
+    fees: 18,
+    avgPackage: 14,
+    exams: ["CAT", "CMAT", "GMAT"]
+  },
+  {
+    id: 4,
+    name: "TAPMI Manipal",
+    tier: "Tier II",
+    location: "Manipal, Karnataka",
+    fees: 19,
+    avgPackage: 13,
+    exams: ["CAT", "NMAT"]
+  }
+];
